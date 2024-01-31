fc8590513c7e37d9c2e496b00c458f5f8a6868be
Kurt-1an <121944031+Kurt-1an@users.noreply.github.com>
2024-01-31 10:59:04 +0800

    Add files via upload

---
+++ ABC.Shared/Models/Store.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ABC.Shared.Models
+{
+    public class Store
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Store Name")]
+        public string storeName { get; set; }
+
+        [Required]
+
+        [DisplayName("Contact Number")]
+        public long storeContactNumber { get; set; }
+
+        [Required]
+        [DisplayName("Email Address")]
+        public string storeEmail { get; set; }
+
+        [Required]
+
+        [DisplayName("Status")]
+        public string storeStatus { get; set; }
+
+        [DisplayName("Lot & Blk no.")]
+        public string storeLotBlk { get; set; }
+
+        [Required]
+        [DisplayName("Street/Subdivision")]
+        public string storeStreetSubdv { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Barangay")]
+        public string storeBarangay { get; set; }
+
+
+        [DisplayName("City")]
+        public string storeCity { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Province")]
+        public string storeProvince { get; set; }
+
+        [Required]
+        [DisplayName("Zip Code")]
+        public int storeZipCode { get; set; }
+    }
+}

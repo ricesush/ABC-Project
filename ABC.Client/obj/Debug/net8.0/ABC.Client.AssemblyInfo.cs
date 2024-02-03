ac9ead5ecd435f3e858da3e551dd689c7ab734ea
ricesush <pixelartme@gmail.com>
2024-02-04 03:17:28 +0800

    4/5 UI for the POS page

--- ABC.Client/obj/Debug/net8.0/ABC.Client.AssemblyInfo.cs
+++ ABC.Client/obj/Debug/net8.0/ABC.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABC.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060b64efe0f2d137b3c02d571ea9923739cfb977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52899690f1107762e151b08f8d22ae6d010d924")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABC.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABC.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

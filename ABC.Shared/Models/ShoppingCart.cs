4bb5291bda9520f7710ac24f862f3ab6a1d002ad
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-02-11 22:35:45 +0800

    OrderHeader at Shopping Cart

--- ABC.Shared/Models/ShoppingCart.cs
+++ ABC.Shared/Models/ShoppingCart.cs
@@ -19,17 +19,16 @@ namespace ABC.Shared.Models
         [ValidateNever]
         public Product Product { get; set; }
 
+        public string ProductName {  get; set; }
+
         [Range(1,100, ErrorMessage = "Please Enter a value between 1 and 20")]
-        public int Count { get; set; }
+        public int Quantity { get; set; }
 
 
         public string? ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
-
-		public double? Charge { get; set; }
-		public double? Discount { get; set; }
 
 		[NotMapped]
         public double Price { get; set; }

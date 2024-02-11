72c8fed4a6025ecd9ec8310e2125055d33a759fc
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-02-12 01:15:51 +0800

    OrderHeader at ShoppingCart

--- ABC.Shared/Models/ShoppingCart.cs
+++ ABC.Shared/Models/ShoppingCart.cs
@@ -10,28 +10,24 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace ABC.Shared.Models
 {
-    public class ShoppingCart
-    {
-        public int Id {  get; set; }
+	public class ShoppingCart
+	{
+		public int Id { get; set; }
+		public int ProductId { get; set; }
+		public string ProductName { get; set; } // Add ProductName property
 
-        public int ProductId { get; set; }
-        [ForeignKey("ProductId")]
-        [ValidateNever]
-        public Product Product { get; set; }
+		// Navigation property for Product
+		[ForeignKey("ProductId")]
+		public Product Product { get; set; }
 
-        [Range(1,100, ErrorMessage = "Please Enter a value between 1 and 20")]
-        public int Count { get; set; }
+		[Range(1, 100, ErrorMessage = "Please Enter a value between 1 and 20")]
+		public int Quantity { get; set; }
 
-
-        public string? ApplicationUserId { get; set; }
-        [ForeignKey("ApplicationUserId")]
-        [ValidateNever]
-        public ApplicationUser ApplicationUser { get; set; }
-
-		public double? Charge { get; set; }
-		public double? Discount { get; set; }
+		public string? ApplicationUserId { get; set; }
+		[ForeignKey("ApplicationUserId")]
+		public ApplicationUser ApplicationUser { get; set; }
 
 		[NotMapped]
-        public double Price { get; set; }
-    }
+		public double Price { get; set; }
+	}
 }

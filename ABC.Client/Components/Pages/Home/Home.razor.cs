ae7b43e4b5084a3876d180b8c837c70dd7beb520
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-02-15 02:14:53 +0800

    inayos po yung ui and yung categories list
    nakaindicate na po yung mga categories pero di pa sya nagfifilter ng products

--- ABC.Client/Components/Pages/Home/Home.razor.cs
+++ ABC.Client/Components/Pages/Home/Home.razor.cs
@@ -1,10 +1,23 @@
-
+using ABC.Client.Data;
+using ABC.Shared.Models;
+using ABC.Shared.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace ABC.Client.Components.Pages.Home;
-public partial class  Home
+public partial class Home
 {
-    protected override void OnInitialized()
+    [Inject] ApplicationDbContext applicationDbContext { get; set; }
+    [Inject] ProductService_SQL categoryService_SQL { get; set; }
+
+    private List<Category> Categories { get; set; } = new List<Category>();
+
+    protected override async Task OnInitializedAsync()
     {
-        
+        var _category = await categoryService_SQL.GetCategoryList(applicationDbContext);
+        if (_category != null && _category.Count > 0)
+        {
+            Categories = _category;
+        }
+        await InvokeAsync(StateHasChanged);
     }
 }

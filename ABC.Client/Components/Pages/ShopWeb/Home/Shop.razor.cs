ada860fd4d18f5cb20536affe35e863224675fe3
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-02-25 19:51:00 +0800

    added search bar

--- ABC.Client/Components/Pages/ShopWeb/Home/Shop.razor.cs
+++ ABC.Client/Components/Pages/ShopWeb/Home/Shop.razor.cs
@@ -15,22 +15,32 @@ public partial class Shop
 	#endregion
 
 	#region fields
-	private List<Product> Products { get; set; } = [];
+	private List<Product> ProductList { get; set; } = [];
 
-	private List<Category> categories = [];
-
-	private Product Product { get; set; } = new();
-	private Category Category { get; set; } = new();
-	
+	private String ProductSearchInput { get; set; } = String.Empty;
 	#endregion
 
 	#region
 	protected override async Task OnInitializedAsync()
 	{
-		Products = await productService_SQL.GetProductList(applicationDbContext);
-		categories = await productService_SQL.GetCategoriesListData(applicationDbContext);
+		productService_SQL.AbcDbConnection = AppSettingsHelper.AbcDbConnection;
+		ProductList = await productService_SQL.GetProductList(applicationDbContext);
+	}
+	private async Task GetProductList(ChangeEventArgs e)
+	{
+		ProductSearchInput = e?.Value?.ToString();
 
-	}	
+		var result = await productService_SQL.GetProductList(applicationDbContext);
+		if (result is not null && result.Count > 0 && !String.IsNullOrEmpty(ProductSearchInput))
+		{
+			ProductList = result.Where(x => x.productName.ToString().Contains(ProductSearchInput, StringComparison.CurrentCultureIgnoreCase)).ToList();
+		}
+		else 
+		{ 
+			ProductList = result.ToList();
+		}
+		await InvokeAsync(StateHasChanged);
+	}
 
 	#endregion
 

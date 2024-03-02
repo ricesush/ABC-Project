af3c85c263e423ba38c9a3f689dc26077502700d
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-03-02 12:40:39 +0800

    homepage

--- ABC.Client/Components/Pages/Home/Home.razor.cs
+++ ABC.Client/Components/Pages/Home/Home.razor.cs
@@ -1,24 +1,74 @@
+using ABC.Client.Components.Pages.Sales_Inventory.Product;
 using ABC.Client.Data;
 using ABC.Shared.Models;
 using ABC.Shared.Services;
 using Microsoft.AspNetCore.Components;
+using MySqlX.XDevAPI.Common;
 
 namespace ABC.Client.Components.Pages.Home;
 public partial class Home
 {
     [Inject] ApplicationDbContext applicationDbContext { get; set; }
-    [Inject] CategoryService_SQL categoryService_SQL { get; set; }
+    [Inject] ProductService_SQL productService_SQL { get; set; }
+    [Inject] OrderHeaderService_SQL orderHeaderService_SQL { get; set; }
 
-    private List<Category> Categories { get; set; } = new List<Category>();
-    private Category categoryData { get; set; }
+    #region fields
+    private List<Product> ProductList { get; set; } = [];
+    private List<OrderDetail> topSellingItems { get; set; } = [];
 
+    #endregion
+
+    #region
     protected override async Task OnInitializedAsync()
     {
-        var _category = await categoryService_SQL.GetCategoryList(applicationDbContext);
-        if (_category != null && _category.Count > 0)
+        productService_SQL.AbcDbConnection = AppSettingsHelper.AbcDbConnection;
+        ProductList = await productService_SQL.GetProductList(applicationDbContext);
+        topSellingItems = await orderHeaderService_SQL.GetOrderDetailsList(applicationDbContext);
+    }
+
+    private async Task GetTopSellingItems()
+    {
+        var topResult = await orderHeaderService_SQL.GetOrderDetailsList(applicationDbContext);
+
+        if (topResult is not null && topResult.Count > 0)
         {
-            Categories = _category;
+            var topSellingItems = topResult
+                .GroupBy(od => od.Product)
+                .OrderByDescending(g => g.Sum(od => od.Count))
+                .Take(3)
+                .Select(g => g.Key)
+                .ToList();
         }
+
         await InvokeAsync(StateHasChanged);
     }
+
+    private async Task GetProductList()
+    {
+        var result = await productService_SQL.GetProductList(applicationDbContext);
+
+        if (result is not null && result.Count > 0)
+        {
+            ProductList = result.OrderByDescending(x => x.DateAdded).ToList();
+        }
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    #endregion
+    //[Inject] ApplicationDbContext applicationDbContext { get; set; }
+    //[Inject] CategoryService_SQL categoryService_SQL { get; set; }
+
+    //private List<Category> Categories { get; set; } = new List<Category>();
+    //private Category categoryData { get; set; }
+
+    //protected override async Task OnInitializedAsync()
+    //{
+    //    var _category = await categoryService_SQL.GetCategoryList(applicationDbContext);
+    //    if (_category != null && _category.Count > 0)
+    //    {
+    //        Categories = _category;
+    //    }
+    //    await InvokeAsync(StateHasChanged);
+    //}
 }

233db192b2cebeeb85636b4381c4bbdc26d5cca2
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-03-04 00:25:09 +0800

    updated order checkout

--- ABC.Client/Components/Pages/ShopWeb/Cart/Summary/Summary.razor.cs
+++ ABC.Client/Components/Pages/ShopWeb/Cart/Summary/Summary.razor.cs
@@ -1,6 +1,60 @@
-﻿namespace ABC.Client.Components.Pages.ShopWeb.Cart.OrderCheckout
+﻿using ABC.Client.Components.Pages.Sales_Inventory.Product;
+using ABC.Client.Data;
+using ABC.Shared.Models;
+using ABC.Shared.Models.ViewModels;
+using ABC.Shared.Services;
+using Microsoft.AspNetCore.Components;
+using Serilog;
+
+namespace ABC.Client.Components.Pages.ShopWeb.Cart.OrderCheckout;
+public partial class Summary
 {
-    public partial class Summary
-    {
-    }
+	#region Injections
+	[Inject] IHttpContextAccessor httpContextAccessor { get; set; }
+	[Inject] ApplicationDbContext applicationDbContext { get; set; }
+	[Inject] ShoppingCartService_SQL shoppingCartService_SQL { get; set; } //papalitan ng vm
+	[Inject] ProductService_SQL productService_SQL { get; set; }
+
+	[Inject] OrderHeaderService_SQL orderHeaderService_SQL { get; set; }
+	#endregion
+
+	#region Fields
+	[CascadingParameter]
+	public HttpContext? HttpContext { get; set; }
+	private List<ShoppingCart> shoppingCartList { get; set; } = [];
+
+	public ShoppingCart shoppingCart { get; set; }
+	public OrderHeader orderHeader { get; set; }
+	public OrderDetail orderDetail { get; set; }
+	public ApplicationUser user { get; set; }
+
+	[SupplyParameterFromForm]
+	public OrderHeader checkoutFormModel { get; set; }
+	#endregion
+
+	protected override async Task OnInitializedAsync()
+	{
+		checkoutFormModel ??= new();
+
+		string userId = httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(x => x.Type.Contains("nameidentifier")).Value;
+		shoppingCartList = await shoppingCartService_SQL.GetShoppingCartList(applicationDbContext, userId);
+
+		//// Create a new order header based on the shopping cart
+		//orderHeader = new OrderHeader
+		//{
+		//	//CustomerId = customer.Id, // Assuming you have a customer object
+		//	//OrderDetails = shoppingCart.Items.Select(cartItem => new OrderDetail
+		//	//{
+		//	//	ProductId = cartItem.Product.Id,
+		//	//	Count = cartItem.Quantity,
+		//	//	// Set other properties like Price, etc.
+		//	//}).ToList()
+		//};
+	}
+
+	private async Task AddOrderHeader()
+	{
+		checkoutFormModel.ApplicationUserId = HttpContext.User.Claims.FirstOrDefault(x => x.Type.Contains("nameidentifier")).Value;
+		bool added = await orderHeaderService_SQL.AddOrderHeader(applicationDbContext, checkoutFormModel);
+	}
 }

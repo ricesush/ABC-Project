945174093204e768f867e5f10d081d5f9a67ef18
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-03-16 00:07:43 +0800

    Add files via upload

---
+++ ABC.Client/Components/Pages/ShopWeb/Cart/CartIndex.razor.cs
@@ -0,0 +1,139 @@
+﻿using ABC.Client.Data;
+using ABC.Shared.Models;
+using ABC.Shared.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using System.Security.Claims;
+using ABC.Shared.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI.Common;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ABC.Client.Components.Pages.ShopWeb.Cart;
+public partial class CartIndex
+{
+
+	#region DEPENDENCY INJECTION
+
+	[Inject] ApplicationDbContext applicationDbContext { get; set; }
+
+	[Inject] ProductService_SQL productService_SQL { get; set; }
+
+	[Inject] ApplicationUserService_SQL applicationUserService_SQL { get; set; }
+
+	[Inject] ShoppingCartService_SQL shoppingCartService_SQL { get; set; }
+
+	[Inject] AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+	[Inject] IHttpContextAccessor HttpContextAccessor { get; set; }
+	[Inject] NavigationManager NavigationManager { get; set; }
+
+
+	#endregion
+
+	#region fields
+
+	[SupplyParameterFromQuery(Name = "product")]
+
+	public string ProductName { get; set; }
+
+	[SupplyParameterFromQuery(Name = "id")]
+
+	public int ProductId { get; set; }
+
+
+
+	private List<ShoppingCart> shoppingCartList { get; set; } = [];
+
+	private Toast toastRef;
+	private string userId;
+	public Product Product { get; set; } = new();
+	public ShoppingCart ShoppingCart { get; set; } = new();
+	public OrderHeader OrderHeader { get; set; } = new();
+	public ShoppingCartVM ShoppingCartVM { get; set; } = new();
+
+	public ApplicationUser ApplicationUser { get; set; } = new();
+	#endregion
+
+	protected override async Task OnInitializedAsync()
+	{
+		productService_SQL.AbcDbConnection = AppSettingsHelper.AbcDbConnection;
+		await LoadProducts();
+	}
+
+	private async Task LoadProducts()
+	{
+		// Get current authenticated user
+		var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+		var user = authState.User;
+
+		if (user.Identity.IsAuthenticated)
+		{
+			var claimsIdentity = user.Identity as ClaimsIdentity;
+			userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+		}
+
+		// Set user id to application User
+		ApplicationUser = await applicationUserService_SQL.GetApplicationUserInfo(applicationDbContext, userId);
+
+		ShoppingCartVM = new()
+		{
+			ShoppingCartList = await shoppingCartService_SQL.GetShoppingCartList(applicationDbContext, userId),
+			OrderHeader = new()
+		};
+
+		UpdateOrderTotal();
+	}
+
+	private async Task AdjustQuantity(ShoppingCart cartItem, int quantityChange)
+	{
+		if ((cartItem.Quantity < cartItem.Product.StockQuantity || quantityChange < 0) && (cartItem.Quantity > 1 || quantityChange > 0))
+		{
+			cartItem.Quantity += quantityChange;
+			bool updated = await shoppingCartService_SQL.UpdateShoppingCart(applicationDbContext, cartItem);
+			await InvokeAsync(StateHasChanged);
+			
+			if (updated)
+			{
+				UpdateOrderTotal();
+			}
+			
+		} else
+		{
+			//Notice
+			if (quantityChange > 0)
+			{
+				toastRef.ShowToast("Warning", "Cannot add more items beyond the available stock quantity!");
+			} else
+			{
+				toastRef.ShowToast("Warning", "Cannot input quantity below 1");
+			}
+			
+		}
+	}
+
+	private async Task PopulateCartDetails(int productId)
+	{
+		ShoppingCart = ShoppingCartVM.ShoppingCartList.FirstOrDefault(c => c.ProductId == productId);
+		await InvokeAsync(StateHasChanged);
+	}
+
+	private async Task Remove(ShoppingCart cartItem)
+	{
+		// Call service to remove product
+		bool removed = await shoppingCartService_SQL.RemoveShoppingCart(applicationDbContext, cartItem);
+		if (removed)
+		{
+			//refresh the list
+			await LoadProducts();
+		}
+	}
+
+	private void UpdateOrderTotal()
+	{
+		ShoppingCartVM.OrderHeader.OrderTotal = ShoppingCartVM.ShoppingCartList.Sum(cart => cart.Product.RetailPrice * cart.Quantity);
+		StateHasChanged(); // Refresh the view
+	}
+
+
+}
---
+++ ABC.Client/Components/Pages/ShopWeb/ManageOrderPage/ManageOrder.razor.cs
@@ -0,0 +1,130 @@
+﻿using ABC.Client.Data;
+using ABC.Shared.Models;
+using ABC.Shared.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
+using ABC.Shared.Utility;
+using System.Security.Claims;
+
+namespace ABC.Client.Components.Pages.ShopWeb.ManageOrderPage;
+
+public partial class ManageOrder
+{
+    #region DEPENDENCY INJECTIOn
+    [Inject] ProductService_SQL productService_SQL { get; set; }
+    [Inject] ApplicationDbContext applicationDbContext { get; set; }
+    [Inject] ApplicationUserService_SQL applicationUserService_SQL { get; set; }
+
+    [Inject] OrderHeaderService_SQL orderHeaderService_SQL { get; set; }
+    [Inject] AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+    [Inject] IHttpContextAccessor HttpContextAccessor { get; set; }
+    [Inject] NavigationManager NavigationManager { get; set; }
+    #endregion
+
+    #region fields
+    private string activeStatus = "all"; // Track the active status
+    private string all { get; set; } = "text-primary";
+    private string pending { get; set; } = "text-primary";
+    private string inprocess { get; set; } = "text-primary";
+    private string shipped { get; set; } = "text-primary";
+    private string cancelled { get; set; } = "text-primary";
+    private string completed { get; set; } = "text-primary";
+
+
+    public HttpContext? HttpContext { get; set; }
+    private string userId;
+    private List<OrderHeader> OrderHeader { get; set; } = new List<OrderHeader>();
+    public ApplicationUser User { get; set; }
+    #endregion
+
+    protected override async Task OnInitializedAsync()
+    {
+        orderHeaderService_SQL.AbcDbConnection = AppSettingsHelper.AbcDbConnection;
+        await LoadProducts();
+        SetActiveStatus(activeStatus);
+    }
+
+    private async Task LoadProducts()
+    {
+        // Get the authenticated user's claims
+        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        // If the user is authenticated, retrieve their user ID from the claims
+        if (user.Identity.IsAuthenticated)
+        {
+            userId = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            // Fetch the orders for the logged-in user
+            OrderHeader = await orderHeaderService_SQL.GetOrderHeaderByUserId(applicationDbContext, userId);
+        }
+
+        FilterOrdersByStatus(activeStatus);
+    }
+
+    private async Task SetActiveStatus(string status)
+    {
+        activeStatus = status;
+        all = "text-primary";
+        pending = "text-primary";
+        inprocess = "text-primary";
+        shipped = "text-primary";
+        cancelled = "text-primary";
+        completed = "text-primary";
+
+        switch (status)
+        {
+            case "pending":
+                pending = "active text-white bg-primary";
+                break;
+            case "inprocess":
+                inprocess = "active text-white bg-primary";
+                break;
+            case "shipped":
+                shipped = "active text-white bg-primary";
+                break;
+            case "cancelled":
+                cancelled = "active text-white bg-primary";
+                break;
+            case "completed":
+                completed = "active text-white bg-primary";
+                break;
+            case "all":
+                all = "active text-white bg-primary";
+                break;
+            default:
+                all = "active text-white bg-primary";
+                break;
+        }
+
+        FilterOrdersByStatus(status);
+    }
+
+    private async Task FilterOrdersByStatus(string status)
+    {
+        OrderHeader = await orderHeaderService_SQL.GetOrderHeaderByUserId(applicationDbContext, userId);
+
+
+        switch (status)
+        {
+            case "pending":
+                OrderHeader = OrderHeader.Where(o => o.OrderStatus == SD.StatusPending).ToList();
+                break;
+            case "inprocess":
+                OrderHeader = OrderHeader.Where(o => o.OrderStatus == SD.StatusProcessing).ToList();
+                break;
+            case "shipped":
+                OrderHeader = OrderHeader.Where(o => o.OrderStatus == SD.StatusShipped).ToList();
+                break;
+            case "cancelled":
+                OrderHeader = OrderHeader.Where(o => o.OrderStatus == SD.StatusCancelled).ToList();
+                break;
+            case "completed":
+                OrderHeader = OrderHeader.Where(o => o.OrderStatus == SD.StatusCompleted).ToList();
+                break;
+            default:
+                break;
+        }
+    }
+}

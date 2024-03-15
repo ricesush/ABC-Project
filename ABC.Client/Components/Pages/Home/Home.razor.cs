08126a6d14466bdd357e44a453c1a9aae0d1ba8b
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-03-16 00:04:51 +0800

    Add files via upload

--- ABC.Client/Components/Pages/Home/Home.razor.cs
+++ ABC.Client/Components/Pages/Home/Home.razor.cs
@@ -1,24 +1,41 @@
+using ABC.Client.Components.Pages.SalesInventory.ProductPage;
 using ABC.Client.Data;
 using ABC.Shared.Models;
 using ABC.Shared.Services;
 using Microsoft.AspNetCore.Components;
+using MySqlX.XDevAPI.Common;
 
 namespace ABC.Client.Components.Pages.Home;
 public partial class Home
 {
     [Inject] ApplicationDbContext applicationDbContext { get; set; }
-    [Inject] CategoryService_SQL categoryService_SQL { get; set; }
+    [Inject] ProductService_SQL productService_SQL { get; set; }
+    [Inject] OrderHeaderService_SQL orderHeaderService_SQL { get; set; }
 
-    private List<Category> Categories { get; set; } = new List<Category>();
-    private Category categoryData { get; set; }
+    #region fields
+    private List<Product> ProductList { get; set; } = [];
+    private List<OrderDetail> topSellingItems { get; set; } = [];
 
+    #endregion
+
+    #region
     protected override async Task OnInitializedAsync()
     {
-        var _category = await categoryService_SQL.GetCategoryList(applicationDbContext);
-        if (_category != null && _category.Count > 0)
+        productService_SQL.AbcDbConnection = AppSettingsHelper.AbcDbConnection;
+        ProductList = await productService_SQL.GetProductList(applicationDbContext);
+    }
+
+    private async Task GetProductList()
+    {
+        var result = await productService_SQL.GetProductList(applicationDbContext);
+
+        if (result is not null && result.Count > 0)
         {
-            Categories = _category;
+            ProductList = result.OrderByDescending(x => x.DateAdded).ToList();
         }
+
         await InvokeAsync(StateHasChanged);
     }
+
+    #endregion
 }

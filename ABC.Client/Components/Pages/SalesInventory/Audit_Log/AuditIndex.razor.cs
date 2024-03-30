05652ea3fb9ec6de3df8cf531f7eb4af35af8e90
Imly2002 <71077444+Imly2002@users.noreply.github.com>
2024-03-31 02:39:46 +0800

    updated audit log

---
+++ ABC.Client/Components/Pages/SalesInventory/Audit_Log/AuditIndex.razor.cs
@@ -0,0 +1,26 @@
+﻿using ABC.Client.Data;
+using ABC.Shared.Models;
+using ABC.Shared.Services;
+using Microsoft.AspNetCore.Components;
+using System.Security.AccessControl;
+
+namespace ABC.Client.Components.Pages.SalesInventory.Audit_Log;
+
+public partial class AuditIndex
+{
+	#region INJECTIONS
+	[Inject] AuditService_SQL AuditService_SQL { get; set; }
+	[Inject] ApplicationDbContext applicationDbContext { get; set; }
+	[Inject] ApplicationUserService_SQL ApplicationUserService_SQL { get; set; }
+	#endregion
+
+	#region FIELDS
+	private List<AuditLog> Audits { get; set; } = [];
+	private ApplicationUser User { get; set; }
+	#endregion
+
+	protected override async Task OnInitializedAsync()
+	{
+        Audits = await AuditService_SQL.GetAuditList(applicationDbContext);
+    }
+}
